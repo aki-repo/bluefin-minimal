a5459124497aac77a24e1f203f36a05e238147be
aki-repo <148870527+aki-repo@users.noreply.github.com>
2025-07-26 13:29:30 -0400

    Create mullvadfixfish.sh

---
+++ files/scripts/mullvadfixfish.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env fish
+
+mkdir -p /tmp/mullvad_extract && cd /tmp/mullvad_extract
+curl -o /tmp/wcurl "https://raw.githubusercontent.com/curl/wcurl/refs/heads/main/wcurl"
+chmod +x /tmp/wcurl
+
+/tmp/wcurl "https://mullvad.net/en/download/app/rpm/latest" --output /tmp/mullvad_extract/mullvad.rpm
+
+ls -la
+echo "rpm2archive NOW"
+rpm2archive mullvad.rpm > mullvad.rpm.tgz
+tar xvf mullvad.rpm.tgz
+ls -la
+
+cp -r opt/Mullvad\ VPN/ /opt/
